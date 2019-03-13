2fdf934877aafe38dd4be04c62ebe14564707d4e
Vaishak Kallore <vaishak.kallore@learnwithflip.com>
2019-03-13 11:04:56 +0530

    Added useCallback hook in menu toggle hooks helper function

--- src/helpers/HooksHelpers.js
+++ src/helpers/HooksHelpers.js
@@ -1,9 +1,9 @@
-import { useState } from 'react'
+import { useState, useCallback } from 'react'
 
 export const useMenuToggle = currentState => {
   const [menuToggle, setMenuToggle] = useState(currentState)
-  const setToggle = () => {
+  const setToggle = useCallback(() => {
     setMenuToggle(!menuToggle)
-  }
+  })
   return [menuToggle, setToggle]
 }
